6ae522f900500dbe046271de2e91f95732dd9998
Farkhad9 <farhadai@code.edu.az>
2026-03-13 23:38:10 +0400

    промисы в js

--- index.js
+++ index.js
@@ -200,18 +200,22 @@
 // }
 
 
-class Comment {
-    constructor(text){
-        this.text = text
-        this.votesQty = 0
-    }
-    upvote(){
-        this.votesQty += 1
-    }
-}
-const firstComment = new Comment('First comment')
-firstComment.upvote()
-console.log(firstComment.votesQty); // 1
-firstComment.upvote()
-console.log(firstComment.votesQty); // 2
+// class Comment {
+//     constructor(text){
+//         this.text = text
+//         this.votesQty = 0
+//     }
+//     upvote(){
+//         this.votesQty += 1
+//     }
+// }
+// const firstComment = new Comment('First comment')
+// firstComment.upvote()
+// console.log(firstComment.votesQty); // 1
+// firstComment.upvote()
+// console.log(firstComment.votesQty); // 2
+
+
+
+
 
